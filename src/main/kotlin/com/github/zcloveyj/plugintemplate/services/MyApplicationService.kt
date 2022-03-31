4b89d0f09f54ceb9142c8b849d6ed21dfab43409
GitHub Action <action@github.com>
2022-03-31 07:10:46 +0000

    Template cleanup

--- src/main/kotlin/com/github/zcloveyj/plugintemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/zcloveyj/plugintemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.zcloveyj.plugintemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.zcloveyj.plugintemplate.MyBundle
 
 class MyApplicationService {
 
